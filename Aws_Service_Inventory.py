a2ece024a6520b1fb23a4347d6cd54175f5466cd
minhaj <minhaj@gmail.com>
2023-02-11 03:24:20 +0000

    Aws commit

--- Aws_Service_Inventory.py
+++ Aws_Service_Inventory.py
@@ -17,6 +17,7 @@ aws_services.append("API")
 aws_services.append("EFS")
 aws_services.append("Cloud9")
 aws_services.append("RDS")
+aws_services.append("X-RAY")
 
 # Print the list and its length
 print("AWS Services: ", aws_services)
